535ce94487740159b4c4e2e8c79dae1c61138c42
Yusuke Sasaki <yuusuke.sasaki.0620@gmail.com>
2024-12-27 16:13:52 +0900

    freeplaneてすと

---
+++ freeplaneテスト.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="freeplaneテスト" FOLDED="false" ID="ID_452131666" CREATED="1610381621610" MODIFIED="1735283167149" STYLE="oval" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6-noEdgeColor.mm" fit_to_viewport="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_207122690" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_207122690" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_3752836">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_3752836"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="あああ" POSITION="bottom_or_right" ID="ID_1811312212" CREATED="1735283175155" MODIFIED="1735283213490" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<node TEXT="まごまご" ID="ID_1824689406" CREATED="1735283413509" MODIFIED="1735283420896"/>
+<node TEXT="bっっｂ" ID="ID_127739315" CREATED="1735283421504" MODIFIED="1735283426140"/>
+</node>
+<node TEXT="いいい" POSITION="top_or_left" ID="ID_1263301960" CREATED="1735283200059" MODIFIED="1735283218106">
+<node TEXT="eえええ" ID="ID_1295300177" CREATED="1735283429009" MODIFIED="1735283432273"/>
+</node>
+<node TEXT="ううう" POSITION="top_or_left" ID="ID_1317840711" CREATED="1735283201384" MODIFIED="1735283223229">
+<node TEXT="っっっｄ" ID="ID_1664973551" CREATED="1735283468449" MODIFIED="1735283475977"/>
+<node TEXT="bっｂ" ID="ID_1058975618" CREATED="1735283476477" MODIFIED="1735283478015"/>
+</node>
+<node TEXT="えええ" POSITION="top_or_left" ID="ID_803800555" CREATED="1735283202403" MODIFIED="1735283346682"/>
+<node TEXT="あああ" POSITION="top_or_left" ID="ID_1145186264" CREATED="1735283519739" MODIFIED="1735283524681"/>
+<node TEXT="aあああ" POSITION="top_or_left" ID="ID_949192607" CREATED="1735283525608" MODIFIED="1735283527905"/>
+<node TEXT="aああああああ" POSITION="top_or_left" ID="ID_282064821" CREATED="1735283528288" MODIFIED="1735283530492">
+<node TEXT="aあああ" ID="ID_179547997" CREATED="1735283531380" MODIFIED="1735283533569">
+<node TEXT="bっっｂ" ID="ID_1441103706" CREATED="1735283533573" MODIFIED="1735283535480">
+<node TEXT="dっっっｃ" ID="ID_120059749" CREATED="1735283535485" MODIFIED="1735283537444">
+<node TEXT="dっっｄ" ID="ID_1832393164" CREATED="1735283537446" MODIFIED="1735283572574"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
